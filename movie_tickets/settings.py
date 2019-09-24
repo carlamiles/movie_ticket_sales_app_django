6991efc8de912b5f402f9b21be01b59157558e61
Carla Miles <ms.carla.albert@gmail.com>
2019-09-23 20:39:57 -0500

    updated settings.py for static files

--- movie_tickets/settings.py
+++ movie_tickets/settings.py
@@ -117,8 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # EMAIL_BACKEND = ‘django.core.mail.backends.smtp.EmailBackend’
 # EMAIL_HOST = ‘smtp.gmail.com’
